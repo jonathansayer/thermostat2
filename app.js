df1efe272599272674b276734339df541ae15346
jonathansayer <jon.sayer1@gmail.com>
2015-07-07 17:54:42 +0100

    powersaver and increase/decrease temp buttons

--- app.js
+++ app.js
@@ -5,5 +5,19 @@ document.getElementById("temperature").innerHTML = thermostat.temp;
 document.getElementById("up").addEventListener("click",  function() {
   thermostat.increase();
   document.getElementById("temperature").innerHTML = thermostat.temp;
-  }
-);
+  });
+
+document.getElementById("down").addEventListener("click", function() {
+  thermostat.decrease();
+  document.getElementById("temperature").innerHTML = thermostat.temp;
+});
+
+
+document.getElementById("reset").addEventListener("click", function() {
+  thermostat.resetTemp();
+  document.getElementById("temperature").innerHTML = thermostat.temp;
+});
+
+document.getElementById('powersaver').addEventListener("click", function() {
+  thermostat.powerSave = !thermostat.powerSave;
+});
