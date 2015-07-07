fbc82400621a8ad431cfdc2467f7adc5a9738c3e
TheoLeanse <theoleanse@gmail.com>
2015-07-07 18:36:57 +0100

    implements changing text colour

--- app.js
+++ app.js
@@ -5,19 +5,25 @@ document.getElementById("temperature").innerHTML = thermostat.temp;
 document.getElementById("up").addEventListener("click",  function() {
   thermostat.increase();
   document.getElementById("temperature").innerHTML = thermostat.temp;
+  changeColour();
   });
 
 document.getElementById("down").addEventListener("click", function() {
   thermostat.decrease();
   document.getElementById("temperature").innerHTML = thermostat.temp;
+  changeColour();
 });
-
 
 document.getElementById("reset").addEventListener("click", function() {
   thermostat.resetTemp();
   document.getElementById("temperature").innerHTML = thermostat.temp;
+  changeColour();
 });
 
 document.getElementById('powersaver').addEventListener("click", function() {
   thermostat.powerSave = !thermostat.powerSave;
 });
+
+var changeColour = function() {
+  document.getElementById('temperature').style.color = thermostat.colour;
+};
